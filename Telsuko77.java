3d7ce3b1c76f8bd7e964a2d2641b9b2d2661d786
Samarththakuri <samarththakuri98@gmail.com>
2025-02-18 20:36:14 +0530

    Add files via upload

---
+++ Telsuko77.java
@@ -0,0 +1,15 @@
+package telsuko;
+public class Telsuko77 {
+    public static void main(String a[]){
+        int i=0,j=0;
+      try{
+        j=18/i;
+      }
+      catch(Exception e){
+        System.out.println("Something went wrong");
+
+      }
+      System.out.println(i);
+      System.out.println("Bye");
+    }
+}
