79b57fa6d5c30d67082d4d226b0945763395c666
Samarththakuri <samarththakuri98@gmail.com>
2025-02-19 21:54:24 +0530

    Add files via upload

---
+++ Customercart.java
@@ -0,0 +1,8 @@
+import tools.Product;
+
+public class Customercart {
+    public static void main(String a[]) {
+        Product prd=new Product("Sauce",25.5,24);
+        prd.getProductDetails();
+    }
+}
