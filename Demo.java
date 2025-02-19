347dbf3ac00d2e831649aa7c9b580d25bf9a836a
Samarththakuri <samarththakuri98@gmail.com>
2025-02-19 21:43:09 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+
+import tools.Student;
+
+public class Demo {
+    public static void main(String a[]){
+        Student stu1=new Student("1234312", "Samarth", "8.3");
+        stu1.dispalyinfo();
+    }
+  
+}
---
+++ tools/Student.java
@@ -0,0 +1,19 @@
+package tools;
+
+public class Student {
+    private String name;
+    private String grade;
+    private String stuid;
+    public Student(String stuid,String name,String grade){
+        this.stuid=stuid;
+        this.grade=grade;
+        this.name=name;
+    }
+    public void dispalyinfo(){
+        System.out.println("Student id is:"+stuid);
+        System.out.println("Nmae:"+name);
+        System.out.println("Grade"+grade);
+        
+    }
+    
+}
